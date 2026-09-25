bdff91f266f7353e7b2a31695443cd84ad5aa1a2
Anuruddh Yadav <anuruddhyadav933@gmail.com>
2026-09-25 23:39:18 +0530

    Enhance audio play functionality with error handling
    Added error handling for audio playback failure.

--- scripttech.js
+++ scripttech.js
@@ -1,7 +1,13 @@
-const audio = new Audio('sound.mp3');
+const audio = new Audio("sound.mp3");
+
 audio.loop = true;
 
-
-document.addEventListener('click', () => {
-    audio.play();
-}, { once: true }); 
+document.addEventListener(
+    "click",
+    function () {
+        audio.play().catch(function () {
+            console.log("Audio could not be played.");
+        });
+    },
+    { once: true }
+);
